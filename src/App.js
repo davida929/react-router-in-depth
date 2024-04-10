b0c30e3be488900bb169cf5736e76d06536c8710
davida929 <davidabertho.93@gmail.com>
2024-04-10 14:16:04 +0000

    basic done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
+    <BrowserRouter>
+      <header>
+        <h1>MyLogo</h1>
+        <nav>
+          <NavLink to='/'> Home </NavLink>
+          <NavLink to='/about'>About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='about' element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <div className='about'>
+      <h2>About</h2>
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do e
+        iusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+      </p>
+    </div>
+  );
+}
+
+export default About;
